46417ec965231d4c76def0acc18d98d2b6beeb33
Yoon Soey <83643907+hiyoons@users.noreply.github.com>
2024-08-21 14:29:40 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        
+        if(my_string.length()<is_prefix.length())
+            return answer;
+        
+        
+        for(int i=0;i<is_prefix.length();i++){
+            if(my_string.charAt(i)==is_prefix.charAt(i))
+                answer=1;
+            else
+                return 0;
+                
+        }
+        
+    
+        return answer;
+    }
+}
