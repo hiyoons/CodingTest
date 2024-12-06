3f9400088bca9e5e51e3d2dc44fe775d52b558c7
Yoon Soey <83643907+hiyoons@users.noreply.github.com>
2024-12-06 10:08:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,14 +1,10 @@
 def solution(a, b, c):
-    answer = 0
-    if a==b:
-        if a==c:
-            answer=(a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
-        else: #ab는 같고 c는 다름
-            answer=(a+b+c)*(a**2+b**2+c**2)
+    answer =a+b+c
+    listToSet=set([a,b,c])
+    if len(listToSet)==1:
+        answer*=(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    elif len(listToSet)==2:
+        answer*=(a**2+b**2+c**2)
     else:
-        #a,b는 다름 
-        if a==c or b==c:
-            answer=(a+b+c)*(a**2+b**2+c**2)
-        else:
-            answer=a+b+c
+        return answer
     return answer
