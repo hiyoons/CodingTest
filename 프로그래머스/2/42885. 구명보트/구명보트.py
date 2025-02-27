669e683213e30852be275eb6c86f214a2d6061c7
Yoon Soey <83643907+hiyoons@users.noreply.github.com>
2025-02-27 18:30:25 +0900

    [level 2] Title: 구명보트, Time: 10.85 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -5,9 +5,17 @@ def solution(people, limit):
     cnt=0
     i=0
     j=len(people)-1
-    while i<=j:   
-        if people[i]+people[j]<=limit:
-            j-=1
-        i+=1
+    while i<j:   
+        if people[i]+people[j]>=limit:
+            if len(people)>0:
+                people.pop(i)
+                i+=1
+        else:
+            if i>=0:
+                people.pop(i)
+                i+=1
+            if j<len(people):
+                people.pop(j)
+                j-=1
         answer+=1
     return answer
